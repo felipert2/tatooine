ce23ed5202b09be2bfcce8a5c97fd66745795dd7
Luis Felipe Ribeiro <44618496+felipert2@users.noreply.github.com>
2023-06-13 20:29:34 -0300

    s

---
+++ Aulas/aula20.js
@@ -0,0 +1,11 @@
+let entrada1 = document.getElementById("1");
+console.log(entrada1);
+
+
+let entrada2 = document.getElementsByClassName("2");
+console.log(entrada2);
+
+
+let entrada3 = document.getElementsByClassName("2");
+console.log(entrada2[1]);
+
